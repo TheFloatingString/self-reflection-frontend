9afcff2adacb1c96e8a42e6b1e5efbe6b9081c3e
The Floating String <laurencel2001@gmail.com>
2023-02-08 16:56:04 -0500

    Create initial reflection user interface

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
+import Button from '@mui/material/Button';
+import Input from '@mui/material/Input';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+	<div className="App">
+		<h1>Self-Reflection</h1>
+	  	<form>
+	  	<label>1. How satisifed are you with your outer apperance and facial features?</label>
+	  	<p></p>
+	  	<Input/>
+		<p></p>
+		<Button variant="outlined">Submit</Button>
+
+		</form>
+	
+	</div>
   );
 }
 

2f4027c18d543905f8936e70e215352e815fc122
The Floating String <laurencel2001@gmail.com>
2023-05-05 20:54:29 -0400

    WIP add Buddhist Sources

--- src/pages/TranslatedScriptsPage.js
+++ src/pages/TranslatedScriptsPage.js
@@ -2,27 +2,112 @@ import React from 'react';
 import '../App.css';
 import NavigationBar from '../components/NavigationBar';
 
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
+
+
 function TranslatedScriptsPage() {
   return (
     <div className='App'>
       <NavigationBar />
-      <h1>Buddhist Sources</h1>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod eros ut risus tincidunt, 
-        id laoreet ipsum vestibulum. Nullam vulputate massa quis risus molestie, ut pulvinar enim 
-        ultricies. Integer vel mauris quam. Sed sagittis libero velit, sit amet bibendum magna 
-        vehicula vel. Donec suscipit tellus sit amet massa vestibulum, eget finibus tellus faucibus. 
-        Donec ut tellus urna. Fusce at elit augue. Sed ac diam non diam mattis varius. 
-      </p>
-      <h2>Bouddhist Sources</h2>
-      <p>
-        Sed vulputate massa sit amet semper hendrerit. Fusce sed eros a ex venenatis dictum in 
-        vel risus. Vivamus hendrerit venenatis neque, eget fringilla magna consectetur quis. 
-        Donec dictum nibh et ex volutpat, vitae tempus enim finibus. Praesent vulputate 
-        vestibulum nisi non feugiat. Nullam a tellus eget lorem pellentesque hendrerit 
-        at at magna. Nam convallis massa vitae porttitor finibus. Aliquam lobortis quam sed 
-        magna facilisis, nec gravida nisl varius. 
-      </p>
+      
+
+        <p>
+        「欲知前世因，今生受者是；欲知未來果，今生作者是。」<br />
+        "To know the cause of your past life, look at your current circumstances. To know the result of your future life, look at your current actions." 
+        </p>
+
+        <p>
+        It highlights the concept of karma, where one's current experiences are the result of past actions and one's future experiences are determined by one's present actions.
+        </p>
+
+        <p> Excerpts from《佛說善惡因果經》: <a href="https://cbetaonline.dila.edu.tw/zh/T2881">https://cbetaonline.dila.edu.tw/zh/T2881</a></p>
+
+        <h2><i>Sūtra on the Cause and Effect of Good and Evil</i></h2>
+
+
+        <TableContainer component={Paper}>
+          <Table>
+            <TableBody>
+              <TableRow>
+                <TableCell>
+                  <p>如是我聞：</p>
+                  <p>一時，佛在舍衛國祇樹給孤獨園。爾時，世尊與無量菩薩、人、天大眾圍繞說法，一心靜聽。</p>
+                  <p>爾時，阿難為眾生故，而白佛言：「世尊！今見世間等同一種生在人中，有好、有醜，有強、有弱，有貧、有富，有苦、有樂，有貴、有賤，音聲不同、言語殊方，有百歲不死、有三十早亡、有十五夭喪、胞胎墮落。</p>
+
+                </TableCell>
+                <TableCell>
+                  <p>Thus have I heard:</p>
+                  <p>At one time, the Buddha was in Shravasti, in the Jeta Grove of Anathapindika. At that time, the Buddha was surrounded by a great assembly of countless bodhisattvas, devas (celestial beings), and humans, all of whom were single-mindedly and quietly listening to his teachings.</p>
+                  <p>At that time, venerable Ānanda, for the sake of sentient beings, approached the Buddha and said, "World-Honored One, I have observed that in this world, there are beings who are born as humans, some are good-looking, some are ugly, some are strong, some are weak, some are poor, some are rich, some experience suffering, some experience happiness, some are noble, some are lowly, their speech and language are different, some live for a hundred years, while some die young at the age of thirty or even earlier at the age of fifteen, and some are aborted fetuses.</p>
+
+
+                </TableCell>
+              </TableRow>
+              <TableRow>
+
+                <TableCell>
+                  <p>「有端政而貧賤、有醜陋而富貴，有大強下劣、有軟弱登上位，有苦而長壽、有樂而命殤，有行善而致過、有作惡而福利，有肥白眼𭾸睞、有青黑而婉媚，有雖短小而足意氣、有雖長大為他僕使。</p>
+
+                 <p>「有豐饒男女、有孤單獨自，有在外遊飢寒憔悴、有入宮在朝衣食自恣，有少時貧賤老始富貴、有理實無辜橫罹獄事，有父慈子孝論經說義、有兄弟乖各鬪諍交至，有安居立宅種種豐備、有自無舍屋處處浮寄。</p>
+
+                 <p>「有烏栖鹿宿𤢌狩同類、有衣毛茹血不識文字，有端坐受報、有客作無地，有聰明高爽、有闇鈍無智，有經營始得、有不求自至，有富而慳貪、有貧窮而好施，有出言和睦、有發語棘刺，有為他愛敬、有眾人遠避，有慈心養命、有殺生無比，有寬而得眾、有為他所棄。</p>
+
+                <p>「有婦姑相憎、有妯娌歡戲，有喜聽法語、有聞經眠睡，有武夫無禮、有好學文義，有作畜生之形種種異類。</p>
+
+                <p>「唯願世尊廣說因果，大眾若聞一心從善。」</p>
+
+                </TableCell>
+
+                <TableCell>
+                  <p>"There are those who are morally upright but poor, and those who are ugly but wealthy. There are those who are strong but lowly, and those who are weak but hold high positions. There are those who suffer but live long, and those who enjoy but die young. There are those who do good but experience misfortune, and those who do evil but receive blessings. There are those with plump, white eyes who are not attractive, and those with dark, green eyes who are beautiful. There are those who are short but possess an indomitable spirit, and those who are tall but are servants to others.</p>
+
+                  <p>"There are men and women who are rich and abundant, and those who are lonely and destitute. Some suffer from hunger and cold while traveling outside, while others enjoy the luxuries of palace life. Some who were once poor and lowly become wealthy and respected in their old age, while others who are innocent and just are imprisoned unfairly. There are fathers who are kind and sons who are filial and who recite sutras and speak of righteous teachings, and there are siblings who quarrel and fight with each other. Some live in comfort and abundance in their own homes, while others are homeless and wander from place to place.</p>
+
+                  <p>"There are those who live in caves or in the forest and hunt for their food like deer and birds, and those who wear animal skins and drink blood but do not know how to read or write. There are those who sit upright and receive good consequences, and those who are homeless and have no place to go. Some are intelligent and clever, while others are dull and lacking in wisdom. Some achieve success through diligent effort, while others attain it without seeking it. There are those who are wealthy but stingy, and those who are poor but generous. Some speak kindly and promote harmony, while others speak harshly and create discord. Some are loved and respected by others, while others are avoided by the masses. There are those who have a kind heart and protect life, and those who kill without measure. Some are generous and win the favor of many, while others are rejected by others.</p>
+
+                  <p>"There are women who hate each other, and those who have a harmonious relationship with their sisters-in-law. Some enjoy listening to the Dharma while others fall asleep while listening to the sutras. Some warriors are rude, while others are interested in studying literature and philosophy. There are many different types of people, some resembling animals in their behavior and appearance.</p>
+
+                  <p>"I only hope that the World Honored One will expound on the law of cause and effect so that all beings can listen to it single-mindedly and practice the way of goodness."</p>
+
+
+                </TableCell>
+
+              </TableRow>
+
+
+              <TableRow>
+                <TableCell>
+
+                  <p>佛告阿難：「如汝所問受報不同者，皆由先世用心不等，是以所受千差萬別。</p>
+
+                  <p><b>今身端政者從忍辱中來，為人醜陋者從瞋恚中來，為人貧窮者從慳貪中來，為人高貴從禮拜中來，為人下賤從憍慢中來，
+                  為人長大從恭敬中來，為人座短從慢法中來，</b></p>
+                  <p><b>…</b></p>
+
+                  <p><b>為人瘖瘂從謗法中來，為人耳聾從不喜聞法中來，為人脣缺從穿魚鰓中來，</b></p>
+                  <p><b>…</b></p>
+                  <p><b>為人心痛病者從斫刺眾生身體中來，</b></p>
+                  <p><b>…</b></p>
+                  <p><b>為人長命者從慈心中來，為人短命者從殺生中來，為人大富者從布施中來，為人聰明從學問誦經中來，為人闇鈍從畜生中來，為人奴婢從[A3]負債中來，</b></p>
+                  <p><b>…</b></p>
+                  <p><b>為人六根具足者從持戒中來，為人諸根不具足者從破戒中來。</b></p>
+                  <p><b>…</b></p>
+                  <p><b>…</b></p>
+
+
+                </TableCell>
+
+              </TableRow>
+            </TableBody>
+          </Table>
+        </TableContainer>
+
+
     </div>
   );
 }

06ea3a096e2f629176de873e4e6f7f3d46d57b02
The Floating String <laurencel2001@gmail.com>
2023-05-06 16:00:47 -0400

    WIP add content to Questions for Pondering page

--- src/pages/FaqPage.js
+++ src/pages/FaqPage.js
@@ -2,6 +2,9 @@ import React, { useState } from 'react';
 import '../App.css';
 import NavigationBar from '../components/NavigationBar';
 
+import List from "@mui/material/List"
+import ListItem from "@mui/material/ListItem"
+
 function FaqPage() {
   const [showQuestion1, setShowQuestion1] = useState(false);
   const [showQuestion2, setShowQuestion2] = useState(false);
@@ -25,61 +28,64 @@ function FaqPage() {
             <h1>Questions for Pondering</h1>
 
 
-「欲知前世因，今生受者是；欲知未來果，今生作者是。」
-"To know the causes of your past lives, look at your current circumstances. To know the consequences of your future lives, look at your current actions." 
+<p>「欲知前世因，今生受者是；欲知未來果，今生作者是。」
+"To know the causes of your past lives, look at your current circumstances. To know the consequences of your future lives, look at your current actions." </p>
 
-It highlights the concept of karma, where one's current experiences are the result of past actions and one's future experiences are determined by one's present actions.
-
-
-Aren’t we all simply direct results of nature and nurture, meaning genetics, lifestyle, personal care and environment, etc.?
-
-How does each life stage reflect our karma from past lives?
-  From birth and living through early childhood, middle childhood and teenage stage (up to 17 years old), we are heavily dependent on our parents for emotional, educational and financial support. This is a direct reflection of the karma we created in past lives. 
-
-  After the age of 17 we start to make decisions that will either benefit or harm other beings and our environment. 
-
-After childhood, we are free to go off on our own/make our own choices
-Better circumstances make it easier to make good choices
-We don’t have control over the situation we start off in, but we can control our decisions in this life
-Determinante (we achieve “the right views” and are able to make progress) vs indeterminate (constantly switching from good to bad and bad to good) personality
+<p>It highlights the concept of karma, where one's current experiences are the result of past actions and one's future experiences are determined by one's present actions.</p>
 
 
+<p><b>Aren’t we all simply direct results of nature and nurture, meaning genetics, lifestyle, personal care and environment, etc.?</b></p>
 
-Physical Quotient
+<p><b>How does each life stage reflect our karma from past lives?</b><br/>
+  From birth and living through early childhood, middle childhood and teenage stage (up to 17 years old), we are heavily dependent on our parents for emotional, educational and financial support. This is a direct reflection of the karma we created in past lives. </p>
 
-- Isn’t it purely genetics that we get from our biological parents? What does it have to do with karma?
-The amount of genetic variation even between two people who have the same parents is HUGE
-Does not just include appearances, also includes things like health, intellect, emotional intellect, etc.
-Common misunderstanding - if one feels one is beautiful, it’s because they deserve it. If they feel they are ugly, they blame their parents. (it's not me, it’s them)
-Relationship is correlated, but that does not mean direct causation
+  <p>After the age of 17 we start to make decisions that will either benefit or harm other beings and our environment. </p>
 
-
-On appearance: If physical beauty is the consequence of having practiced patience in past lives, how come some beautiful people are obnoxious and impatient?
-
-On health and longevity: If health and longevity is the consequence of having protected beings (humans and non-humans) in past lives, how come some healthy people are cruel and harmful to beings.
-
-
-Intellectual Quotient
-
-Isn’t IQ a direct product of our environment?
+<ul>
+<li>After childhood, we are free to go off on our own/make our own choices</li>
+<li>Better circumstances make it easier to make good choices</li>
+<li>We don’t have control over the situation we start off in, but we can control our decisions in this life</li>
+<li><b>Determinante</b> (we achieve “the right views” and are able to make progress) vs <b>indeterminate</b> (constantly switching from good to bad and bad to good) personality</li>
+</ul>
 
 
-Emotional Quotient
 
-Is personality hard-wired or can it be altered?
-Can emotional management ability be acquired during our lifetime, or is it innate?
+<p><b>Physical Quotient</b></p>
+
+<p>- Isn’t it purely genetics that we get from our biological parents? What does it have to do with karma?</p>
+<ul>
+  <li>The amount of genetic variation even between two people who have the same parents is HUGE</li>
+  <li>Does not just include appearances, also includes things like health, intellect, emotional intellect, etc.</li>
+  <li>Common misunderstanding - if one feels one is beautiful, it’s because they deserve it. If they feel they are ugly, they blame their parents. (it's not me, it’s them)</li>
+  <li>Relationship is correlated, but that does not mean direct causation</li>
+</ul>
+
+<p>On appearance: If physical beauty is the consequence of having practiced patience in past lives, how come some beautiful people are obnoxious and impatient?</p>
+
+<p>On health and longevity: If health and longevity is the consequence of having protected beings (humans and non-humans) in past lives, how come some healthy people are cruel and harmful to beings.</p>
 
 
-Spiritual Quotient
+<p><b>Intellectual Quotient</b></p>
 
-Is spirituality an optional or essential aspect of life?
-Can one be happy and at peace in life without being spiritual?
+<p>Isn’t IQ a direct product of our environment?</p>
 
---------------------------
 
-FAQ on Karma and Consequence
+<p><b>Emotional Quotient</b></p>
 
-If generosity from past lives leads to wealth in the present life, how come some rich people are notably stingy? 
+<p>Is personality hard-wired or can it be altered? <br />
+Can emotional management ability be acquired during our lifetime, or is it innate?</p>
+
+
+<p><b>Spiritual Quotient</b></p>
+
+<p>Is spirituality an optional or essential aspect of life? <br />
+Can one be happy and at peace in life without being spiritual?</p>
+
+<p>--------------------------</p>
+
+<p><b>FAQ on Karma and Consequence</b></p>
+
+<p>If generosity from past lives leads to wealth in the present life, how come some rich people are notably stingy? </p>
 
 
 

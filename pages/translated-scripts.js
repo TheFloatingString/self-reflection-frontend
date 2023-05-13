e7abc5f767eca3b58ea1d0c699b5e6904cbafb75
The Floating String <laurencel2001@gmail.com>
2023-05-13 12:23:22 -0400

    Add Buddhist Sources

--- pages/translated-scripts.js
+++ pages/translated-scripts.js
@@ -1,7 +1,271 @@
+import { NextUIProvider } from "@nextui-org/react";
+import { Grid, Card, Text } from "@nextui-org/react";
+import { Container, Row, Col } from "@nextui-org/react";
+
+
 export default function translatedScripts() {
+
+
+	const sourceText = ({ text }) => {
+		return (
+				<Card>
+					<Card.Body>
+						<Text>
+							{text}
+						</Text>
+					</Card.Body>
+				</Card>
+			)
+	}
+
 	return (
 		<div className="main">
-			<h1>Buddhist Sources</h1>
+			<NextUIProvider>
+				<h1>Buddhist Sources</h1>
+
+
+				<p>
+				「欲知前世因，今生受者是；<br/> 
+				欲知未來果，今生作者是。」<br />
+				"To know the cause of your past life, look at your current circumstances. <br/>
+				To know the result of your future life, look at your current actions." 
+				</p>
+
+				<br/>
+
+				<p>
+				It highlights the concept of karma, where one's current experiences are the result of past actions and one's future experiences are determined by one's present actions.
+				</p>
+
+				<p> Excerpts from《佛說善惡因果經》: <a href="https://cbetaonline.dila.edu.tw/zh/T2881">https://cbetaonline.dila.edu.tw/zh/T2881</a></p>
+
+				<h2><i>Sūtra on the Cause and Effect of Good and Evil</i></h2>
+
+				<Container gap={0.1} justify="center">
+					<Row gap={0.5}>
+						<Col>
+
+							<p>如是我聞：</p>
+							<p>一時，佛在舍衛國祇樹給孤獨園。爾時，世尊與無量菩薩、人、天大眾圍繞說法，一心靜聽。</p>
+							<p>爾時，阿難為眾生故，而白佛言：「世尊！今見世間等同一種生在人中，有好、有醜，有強、有弱，有貧、有富，有苦、有樂，有貴、有賤，音聲不同、言語殊方，有百歲不死、有三十早亡、有十五夭喪、胞胎墮落。</p>
+
+						</Col>
+						<Col>
+
+							<p>Thus have I heard:</p>
+							<p>At one time, the Buddha was in Shravasti, in the Jeta Grove of Anathapindika. At that time, the Buddha was surrounded by a great assembly of countless bodhisattvas, devas (celestial beings), and humans, all of whom were single-mindedly and quietly listening to his teachings.</p>
+							<p>At that time, venerable Ānanda, for the sake of sentient beings, approached the Buddha and said, "World-Honored One, I have observed that in this world, there are beings who are born as humans, some are good-looking, some are ugly, some are strong, some are weak, some are poor, some are rich, some experience suffering, some experience happiness, some are noble, some are lowly, their speech and language are different, some live for a hundred years, while some die young at the age of thirty or even earlier at the age of fifteen, and some are aborted fetuses.</p>
+
+						</Col>
+					</Row>
+					<br/><br/>
+					<Row gap={0.5}>
+						<Col>
+							<p>「有端政而貧賤、有醜陋而富貴，有大強下劣、有軟弱登上位，有苦而長壽、有樂而命殤，有行善而致過、有作惡而福利，有肥白眼𭾸睞、有青黑而婉媚，有雖短小而足意氣、有雖長大為他僕使。</p>
+
+							<p>「有豐饒男女、有孤單獨自，有在外遊飢寒憔悴、有入宮在朝衣食自恣，有少時貧賤老始富貴、有理實無辜橫罹獄事，有父慈子孝論經說義、有兄弟乖各鬪諍交至，有安居立宅種種豐備、有自無舍屋處處浮寄。</p>
+
+							<p>「有烏栖鹿宿𤢌狩同類、有衣毛茹血不識文字，有端坐受報、有客作無地，有聰明高爽、有闇鈍無智，有經營始得、有不求自至，有富而慳貪、有貧窮而好施，有出言和睦、有發語棘刺，有為他愛敬、有眾人遠避，有慈心養命、有殺生無比，有寬而得眾、有為他所棄。</p>
+
+							<p>「有婦姑相憎、有妯娌歡戲，有喜聽法語、有聞經眠睡，有武夫無禮、有好學文義，有作畜生之形種種異類。</p>
+
+							<p>「唯願世尊廣說因果，大眾若聞一心從善。」</p>
+						</Col>
+						<Col>
+							<p>"There are those who are morally upright but poor, and those who are ugly but wealthy. There are those who are strong but lowly, and those who are weak but hold high positions. There are those who suffer but live long, and those who enjoy but die young. There are those who do good but experience misfortune, and those who do evil but receive blessings. There are those with plump, white eyes who are not attractive, and those with dark, green eyes who are beautiful. There are those who are short but possess an indomitable spirit, and those who are tall but are servants to others.</p>
+
+							<p>"There are men and women who are rich and abundant, and those who are lonely and destitute. Some suffer from hunger and cold while traveling outside, while others enjoy the luxuries of palace life. Some who were once poor and lowly become wealthy and respected in their old age, while others who are innocent and just are imprisoned unfairly. There are fathers who are kind and sons who are filial and who recite sutras and speak of righteous teachings, and there are siblings who quarrel and fight with each other. Some live in comfort and abundance in their own homes, while others are homeless and wander from place to place.</p>
+
+							<p>"There are those who live in caves or in the forest and hunt for their food like deer and birds, and those who wear animal skins and drink blood but do not know how to read or write. There are those who sit upright and receive good consequences, and those who are homeless and have no place to go. Some are intelligent and clever, while others are dull and lacking in wisdom. Some achieve success through diligent effort, while others attain it without seeking it. There are those who are wealthy but stingy, and those who are poor but generous. Some speak kindly and promote harmony, while others speak harshly and create discord. Some are loved and respected by others, while others are avoided by the masses. There are those who have a kind heart and protect life, and those who kill without measure. Some are generous and win the favor of many, while others are rejected by others.</p>
+
+							<p>"There are women who hate each other, and those who have a harmonious relationship with their sisters-in-law. Some enjoy listening to the Dharma while others fall asleep while listening to the sutras. Some warriors are rude, while others are interested in studying literature and philosophy. There are many different types of people, some resembling animals in their behavior and appearance.</p>
+
+							<p>"I only hope that the World Honored One will expound on the law of cause and effect so that all beings can listen to it single-mindedly and practice the way of goodness."</p>
+
+						</Col>
+					</Row>
+					<br/><br/>
+
+					<Row gap={0.5}>
+						<Col>
+
+							<p>佛告阿難：「如汝所問受報不同者，皆由先世用心不等，是以所受千差萬別。</p>
+
+							<p><b>今身端政者從忍辱中來，為人醜陋者從瞋恚中來，為人貧窮者從慳貪中來，為人高貴從禮拜中來，為人下賤從憍慢中來，
+							為人長大從恭敬中來，為人座短從慢法中來，</b></p>
+							<p><b>…</b></p>
+
+							<p><b>為人瘖瘂從謗法中來，為人耳聾從不喜聞法中來，為人脣缺從穿魚鰓中來，</b></p>
+							<p><b>…</b></p>
+							<p><b>為人心痛病者從斫刺眾生身體中來，</b></p>
+							<p><b>…</b></p>
+							<p><b>為人長命者從慈心中來，為人短命者從殺生中來，為人大富者從布施中來，為人聰明從學問誦經中來，為人闇鈍從畜生中來，為人奴婢從[A3]負債中來，</b></p>
+							<p><b>…</b></p>
+							<p><b>為人六根具足者從持戒中來，為人諸根不具足者從破戒中來。</b></p>
+							<p><b>…</b></p>
+							<p><b>…</b></p>
+
+						</Col>
+						<Col>
+
+							<p>Buddha told Ānanda: "Those who receive different karmic consequences do so because their intentions in past lives were different. Therefore, the consequences that every sentient being experiences are varied and diverse."</p>
+
+							<p>Those who have a beautiful body came from practicing patience.<br/> 
+							Those who are ugly came from anger. Those who are poor came from stinginess.<br/>
+							Those who have a high social status came from reverence, while those who are inferior came from arrogance. <br/>
+							Those who are tall came from respect, while those who are short came from disrespect.
+							                 </p>
+
+
+							<p>Those who are mute came from slandering the teachings of Truth, while those who are deaf came from disliking to hear the Truth. </p>
+
+
+							<p>Those with cleft lips came from piercing the gills of fish. <br/> 
+							Those with heart disease came from torturing and stabbing sentient beings.</p>
+
+							<p>Those with a long life came from practicing kindness, while those with a short life came from killing. <br/>
+							Those who are very rich came from practicing generosity.<br/> 
+							Those who are intelligent came from studying and reciting sutras, while those who are slow came from past lives as animals.<br/>
+							Those who are slaves came from owing debts.</p> 
+
+
+							<p>Those who have complete sensory organs came from having observed precepts, while those with sensory deficiencies came from having broken precepts.</p>
+
+						</Col>
+					</Row>
+
+					<br/><br/>
+
+					<Row gap={0.5}>
+						<Col>
+
+							<p>佛告阿難：「如向所說種種眾苦，皆由十惡之業，上者地獄因緣，中者畜生因緣，下者餓鬼因緣。</p>
+
+							<p>於中殺生之罪，能令眾生墮於地獄、畜生、餓鬼；若生人中得二種果報：一者、短命；二者、多病。</p>
+
+							<p>劫盜之罪...；若生人中得二種果報：一者、貧窮；二者、共財不得自在。</p>
+
+							<p>邪婬之罪...；若生人中得二種果報：一者、婦不貞良；二者、二妻相諍不隨己心。</p>
+
+							<p>妄語之罪...；若生人中得二種果報：一者、多被誹謗；二者、恒為多人所誑。</p>
+
+							<p>兩舌之罪...；若生人中得二種果報：一者、得破壞眷屬；二者、得弊惡眷屬。</p>
+
+							<p>惡口之罪...；若生人間得二種果報：一者、常聞惡聲；二者、所有言說恒有諍訟。</p>
+
+							<p>綺語之罪...；若生人間有二種果報：一者、說正人不言受；二者、所有言說不能辯了。</p>
+
+							<p>貪慾之罪...；若生人中得二種果報：一者、貪財無有厭足；二者、多求恒無從意。</p>
+
+							<p>瞋恚之罪...；若生人中得二種果報：一者、常為他人求其長短；二者、常為他所惱害。</p>
+
+							<p>邪見之罪...；若生人間得二種果報：一者、常生邪見家；二者、心恒諂曲。</p>
+
+						</Col>
+						<Col>
+
+
+							<p>Buddha told Ānanda: "All the sufferings that people face are the result of the ten bad actions. Those who commit serious bad actions will go to hell. Those who commit less serious bad actions are reborn as animals. Those who commit the least of bad actions are reborn as hungry ghosts.</p>
+
+							<p>The transgression of <b>killing</b> can cause beings to be reborn in the realms of hell, animals, or hungry ghosts. 
+							The karmic consequences for those who are born as humans include: (1) living a short life, and (2) suffering many illnesses.</p>
+
+							<p>The transgression of <b>stealing</b> has two karmic consequences: (1) poverty, and (2) lack the freedom to enjoy wealth.</p>
+
+
+							<p>The transgression of <b>sexual misconduct</b> has two karmic consequences: (1) an unfaithful spouse, and (2) always being in conflict and disharmony with their partner.</p>
+
+
+							<p>The transgression of <b>lying</b> has two karmic consequences: (1) being frequently defamed, and (2) constantly being deceived by many people."</p>
+
+
+
+							<p>The transgression of <b>divisive speech</b> has two karmic consequences: (1) having destructive family relationships and (2) having harmful relatives.</p>
+
+
+
+							<p>The transgression of <b>malicious speech</b> has two karmic consequences: (1) frequently hearing malicious words and (2) having all one's words result in conflicts.</p>
+
+
+							<p>The transgression of <b>frivolous speech</b> has two karmic consequences: (1) one’s words, even though correct, will not be accepted by people, and (2) one’s words cannot be explained and defended.</p>
+
+
+							<p>The transgression of <b>being greedy</b> has two karmic consequences: (1) will crave for wealth and never feeling content; (2) will crave insatiably after things beyond reach.​</p>
+
+
+							<p>The transgression of <b>being angry</b> has two karmic consequences: (1) will always be accused of faults; (2) will always be afflicted and harmed by others.</p>​
+
+
+							<p>The transgression of <b>holding wrong views</b> has two karmic consequences: (1) always be born in a family with wrong views; (2) the mind will always be crooked and deceitful (using flattery).​"</p>
+
+
+						</Col>
+					</Row>
+				</Container>
+
+
+			<p>Excerpts from《佛說輪轉五道罪福報應經》: <a href="https://cbetaonline.dila.edu.tw/zh/T0747b">https://cbetaonline.dila.edu.tw/zh/T0747b</a></p>
+
+			<h2><i>Sūtra on the Karma and Consequence of Rebirth in the Five Realms
+			</i></h2>
+
+			<Container>
+				<Row>
+					<Col>
+
+						<p>佛語阿難：「吾觀天地萬物，各有宿緣。」</p>
+						<p>阿難即前為佛作禮，長跪白佛言：「何等宿緣？此諸弟子願欲聞之，唯具演說，開化未聞！」</p>
+						<p>佛告阿難：「善哉！善哉！若樂聞者，[3]心善聽。」</p>
+
+					</Col>
+					<Col>
+
+						<p>Buddha said to Ānanda, "I see that all things in the universe have their own karma from the past." <br/>
+						Ānanda then paid homage to the Buddha and asked, "What is this karma, and all these disciples wish to hear about it. Please explain and enlighten us." </p>
+
+						<p>Buddha replied, "Good, good! If one is pleased to hear, listen well with a good heart."</p>
+
+
+					</Col>
+				</Row>
+				<br/><br/>
+
+				<Row>
+					<Col>
+						<p>2. 人如巨富，財寶無限，富敵侯王者，從夙世 慷慨布施，樂善不倦中來。</p>
+
+						<p>3. 人如長壽，身體康強，毫無病苦者，從夙生 嚴修大戒，持齋戒殺中來。</p>
+
+						<p>4. 人如品貌超越，性情溫和，令人生敬者，從夙生 度量寬宏，忍辱不校中來。</p>
+
+						<p>5. 人如樂修福慧，不憍不快，始終不懈者，從夙生 參求大道，勇猛精進中來。</p>
+
+						<p>6. 人如氣度安詳，沈靜寡言善於審察，不輕舉動者，從夙生修習禪定，心不妄動中來。</p>
+
+						<p>7. 人如開明特達，善解深法，讚歎妙義，開悟愚蒙，人間其言，莫不向化者，從夙生修般若德，燃智慧炬中來。</p>
+
+
+					</Col>
+					<Col>
+						<p>If a person is extremely rich and prosperous, possessing countless treasures and able to rival dukes and kings, it is because in past lives that person has been generous and tireless in giving to others and doing good.</p>
+
+						<p>If a person has a long life and a strong body without illness, it is because in past lives that person has rigorously followed the precepts and abstained from killing or harming other living beings.</p>
+
+						<p>If a person has an attractive appearance, a gentle and respectful personality, it is because in past lives that person has been generous, tolerant, and patient.</p>
+
+						<p>If a person is happy to cultivate merit and wisdom, without being arrogant or complacent, always striving without rest, it is because in past lives that person has diligently sought the path of the Great Vehicle with courage and diligence.</p>
+
+						<p>If a person is composed and calm, quiet with few words but good at observing and does not act impulsively, it is because in past lives that person has cultivated meditation and concentration, and thus his mind remains undisturbed.</p>
+
+						<p>If a person has a brilliant mind and deep understanding of the teachings, able to praise wisdom and enlighten the ignorant, with all their words influencing others, it is because in past lives that person has cultivated the wisdom of Prajñā, igniting the flame of wisdom.</p>
+
+					</Col>
+				</Row>
+			</Container>
+
+
+
+			</NextUIProvider>
 		</div>
 	);
 }
